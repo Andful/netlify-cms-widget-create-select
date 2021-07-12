dd041ae9d356b2a7de5bad2c8b0782f5994de7b5
Andrea Nardi <buongiorno19972@gmail.com>
2021-07-12 17:20:39 +0200

    major refactor and error handling

--- src/Control.tsx
+++ src/Control.tsx
@@ -1,30 +1,34 @@
 import React from 'react';
-import { Creatable }  from 'react-select';
+import { Creatable } from 'react-select';
 // @ts-ignore
 import { reactSelectStyles } from 'netlify-cms-ui-default';
 // @ts-ignore
 import { validations } from 'netlify-cms-lib-widgets';
 import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
-import { List } from 'immutable';
+import { fromJS, List } from 'immutable';
 
-interface GetOptionResult {
-  options: SelectOption[],
-  noOptionMessage: string,
+
+class NoOptionsError extends Error {
+  constructor(message: string) {
+    super(message);
+  }
+  get isNoOptionsError(): boolean {
+    return true;
+  }
 }
 
 interface SelectOption {
-  value: string,
-  label: string,
+  value: string;
+  label: string;
 }
 
 interface Config {
-  mode: string | undefined,
-  url: string | undefined,
-  query: string | undefined,
-  attribute: string | undefined,
-  filter: string | undefined,
-  capture: string | undefined,
+  url: string | undefined;
+  query: string | undefined;
+  attribute: string | undefined;
+  filter: string | undefined;
+  capture: string | undefined;
 }
 
 abstract class Loader {
@@ -40,166 +44,170 @@ abstract class Loader {
       this.capture = new RegExp(this.config.capture);
     }
   }
-  async fetchData(url: string): Promise<Response> {
-    return await fetch(url);
-  }
-  abstract parse(_response: Response): Promise<string[]>;
 
-  async getOptions(): Promise<GetOptionResult> {
-    let response : Response | null = null;
-    let noOptionMessage = 'No options';
-
-    if (this.config.mode === undefined) {
-      return { options: [], noOptionMessage: `No options due to missing parameter "mode"`}
-    }
-
+  async getOptions(): Promise<SelectOption[]> {
     if (this.config.url === undefined) {
-      return { options: [], noOptionMessage: `No options due to missing parameter "url"`}
+      throw new NoOptionsError('No options due to missing field "url"');
     }
 
-    if ((this.config.mode === "html" || this.config.mode === "xml") && this.config.query === undefined) {
-      return { options: [], noOptionMessage: `No options due to missing parameter "query" which is required in "html" and "xml" mode`}
+    let response = await fetch(this.config.url);
+    if (response.status !== 200) {
+      throw new NoOptionsError(
+        `No options due to response status ${response.status}`
+      );
     }
-
-    try {
-      response = await this.fetchData(this.config.url);
-      if (response.status !== 200) {
-        noOptionMessage = `No options due to response code ${response.status}`
-      }
-    } catch(e) {
-      noOptionMessage = `No options due to: "${e}"`
-    }
-  
-    let options: string[] = response !== null ? await this.parse(response) : [];
+    let options = await this.getLoaderOptions(response);
 
     if (this.filter !== undefined) {
-      options = options.filter(o => o.match(this.filter as RegExp) !== null)
+      options = options.filter(o => o.match(this.filter as RegExp) !== null);
+    }
+    if (this.capture !== undefined) {
+      options = options
+        .map(o => o.match(this.capture as RegExp))
+        .filter(m => m !== null)
+        .map(m => (m as RegExpMatchArray)[0]);
     }
 
-    if (this.capture !== undefined) {
-      options = options.map(o => o.match(this.capture as RegExp)).filter(m => m !== null).map(m => (m as RegExpMatchArray)[0])
-    }
-    
-    options.sort((a, b) => a.toLowerCase() < b.toLowerCase() ? -1 : +1);
-    return { options: options.map(value => ({value, label: value})), noOptionMessage};
+    return options.map(v => ({ value: v, label: v }));
   }
+
+  abstract getLoaderOptions(_response: Response): Promise<string[]>;
 }
 
 class XMLLoader extends Loader {
-  async parse(response: Response): Promise<string[]> {
+  async getLoaderOptions(response: Response): Promise<string[]> {
     let parser = new DOMParser();
-    let xmlDoc = parser.parseFromString(
-      await response.text(),
-      'text/xml'
-    );
+    let xmlDoc = parser.parseFromString(await response.text(), 'text/xml');
 
     if (this.config.query === undefined) {
-      return [];
+      throw new NoOptionsError(
+        'No options due to missing field "query" which is required for mode: "xml"'
+      );
     }
 
     let elements = [...xmlDoc.querySelectorAll(this.config.query)];
-    return elements.map(e => {
-      if (this.config.attribute !== undefined) {
-        let value = e.getAttribute(this.config.attribute);
-        if (value === null) {
-          value = "";
+    let options = elements
+      .map(e => {
+        if (this.config.attribute !== undefined) {
+          let value = e.getAttribute(this.config.attribute);
+          if (value === null) {
+            return undefined;
+          }
+          return value as string;
+        } else {
+          let value = e.textContent;
+          if (value === null) {
+            return undefined;
+          }
+          return value as string;
         }
-        return value;
-      } else {
-        let value = e.textContent;
-        if (value === null) {
-          value = "";
-        }
-        return value;
-      }
-    })
+      })
+      .filter(v => v !== undefined) as string[];
+
+    return options;
   }
 }
 
 class HTMLLoader extends Loader {
-  async parse(response: Response): Promise<string[]> {
+  async getLoaderOptions(response: Response): Promise<string[]> {
     let parser = new DOMParser();
-    let xmlDoc = parser.parseFromString(
-      await response.text(),
-      'text/html'
-    );
+    let htmlDoc = parser.parseFromString(await response.text(), 'text/html');
 
     if (this.config.query === undefined) {
-      return [];
+      throw new NoOptionsError(
+        'No options due to missing field "query" which is required for mode: "html"'
+      );
     }
 
-    let elements = [...xmlDoc.querySelectorAll(this.config.query)];
-    return elements.map(e => {
-      if (this.config.attribute !== undefined) {
-        return e.getAttribute(this.config.attribute);
-      } else {
-        return e.textContent;
-      }
-    }).filter(e => e !== null) as string[]
+    let elements = [...htmlDoc.querySelectorAll(this.config.query)];
+    let options = elements
+      .map(e => {
+        if (this.config.attribute !== undefined) {
+          let value = e.getAttribute(this.config.attribute);
+          if (value === null) {
+            return undefined;
+          }
+          return value as string;
+        } else {
+          let value = e.textContent;
+          if (value === null) {
+            return undefined;
+          }
+          return value as string;
+        }
+      })
+      .filter(v => v !== undefined) as string[];
+
+    return options;
   }
 }
 
 class JSONLoader extends Loader {
-  async parse(response: Response): Promise<string[]> {
-    let obj = await response.json();
+  async getLoaderOptions(response: Response): Promise<string[]> {
+    let originalObj = await response.json();
+
+    let obj = fromJS(originalObj);
 
     if (this.config.query !== undefined) {
-      this.config.query.split('.').forEach(q => {
-        if (obj !== undefined) {
-          obj = obj[q];
+      obj = obj.getIn(this.config.query);
+    }
+    if (!List.isList(obj)) {
+      throw new NoOptionsError(
+        `No options due to "${
+          this.config.query === undefined ? '.' : this.config.query
+        }" not being an array in json ${JSON.stringify(originalObj)}`
+      );
+    }
+    obj = obj as List<any>;
+    let options = obj
+      .map((e: any) => {
+        if (this.config.attribute !== undefined) {
+          return e.getIn(this.config.attribute.split('.'));
         }
-      })
-    }
-    if (!Array.isArray(obj)) {
-      throw new Error(`"${this.config.query === undefined ? "." : this.config.query}" is not an array in json ${JSON.stringify(obj)}`)
-    }
-    obj = obj as any[];
-    return obj.map((e: any) => {
-      if (this.config.attribute !== undefined) {
-        this.config.attribute.split('.').forEach(attr => {
-          if (e !== undefined) {
-            e = e[attr];
-          }
-        })
-      }
-      return e;
-    })
-    .filter((e: any) => e !== undefined)
-    .map((e: any) => {
-      if (typeof e === "string") {
         return e;
-      } else {
-        return JSON.stringify(e);
-      }
-    });
+      })
+      .filter((e: any) => e !== undefined)
+      .map((e: any) => {
+        if (typeof e === 'string') {
+          return e;
+        } else {
+          return JSON.stringify(e);
+        }
+      }) as string[];
+    return options;
   }
 }
 
 class PlainLoader extends Loader {
-  async parse(response: Response): Promise<string[]> {
+  async getLoaderOptions(response: Response): Promise<string[]> {
     let text = await response.text();
-    return text.split("\n").filter(e => e !== "");
+    let options = text.split('\n').filter(e => e !== '');
+    return options;
   }
 }
 
-
+class NoModeLoader extends Loader {
+  async getLoaderOptions(): Promise<string[]> {
+    throw new NoOptionsError('No options due to missing field "mode"');
+  }
+}
 
 interface Props {
-  onChange: (_value: List<string> | string | undefined) => void,
-  forID: string,
-  value: List<string> | string,
-  field: Map<string, any>,
-  classNameWrapper: string,
-  setActiveStyle: () => void,
-  setInactiveStyle: () => void,
-  hasActiveStyle: () => void,
-  t: (_a: string, _b: any) => string,
+  onChange: (_value: List<string> | string | undefined) => void;
+  forID: string;
+  value: List<string> | string;
+  field: Map<string, any>;
+  classNameWrapper: string;
+  setActiveStyle: () => void;
+  setInactiveStyle: () => void;
+  hasActiveStyle: () => void;
+  t: (_a: string, _b: any) => string;
 }
 
 interface State {
-  isLoading: boolean,
-  noOptionMessage: string,
-  options: Map<string,SelectOption>
+  isLoading: boolean;
+  hasNoOptionMessage: string | null;
+  options: Map<string, SelectOption>;
 }
 
 export default class Control extends React.Component<Props, State> {
@@ -217,7 +225,11 @@ export default class Control extends React.Component<Props, State> {
 
   constructor(props: Props) {
     super(props);
-    this.state = { isLoading: true, noOptionMessage: "No options", options: new Map() };
+    this.state = {
+      isLoading: true,
+      hasNoOptionMessage: null,
+      options: new Map(),
+    };
   }
 
   isValid = () => {
@@ -236,16 +248,15 @@ export default class Control extends React.Component<Props, State> {
       field.get('label') || field.get('name'),
       value,
       min,
-      max,
+      max
     );
 
     return error ? { error } : { error: false };
   };
 
-
-
   async componentDidMount() {
-    const { field } = this.props;
+    const { value, field, onChange } = this.props;
+    const isMultiple = (field.get('multiple') || false) as boolean;
     const url = field.get('url');
     const mode = field.get('mode');
     const query = field.get('query');
@@ -253,19 +264,36 @@ export default class Control extends React.Component<Props, State> {
     const filter = field.get('filter');
     const capture = field.get('capture');
 
+    if (isMultiple) {
+      if (value === undefined || value === null) {
+        onChange(List());
+      } else {
+        if (List.isList(value)) {
+          onChange(value);
+        } else {
+          onChange(fromJS([value]));
+        }
+      }
+    } else {
+      if (value === undefined || value === null) {
+        onChange(undefined);
+      } else {
+        onChange(value);
+      }
+    }
+
     const config: Config = {
       url,
-      mode,
       query,
       attribute,
       filter,
       capture,
-    }
+    };
 
     let loader: Loader;
 
     if (mode === 'xml') {
-     loader = new XMLLoader(config);
+      loader = new XMLLoader(config);
     } else if (mode === 'html') {
       loader = new HTMLLoader(config);
     } else if (mode === 'json') {
@@ -273,15 +301,25 @@ export default class Control extends React.Component<Props, State> {
     } else if (mode === 'plain') {
       loader = new PlainLoader(config);
     } else {
-      loader = new PlainLoader(config);
+      loader = new NoModeLoader(config);
     }
 
     let optionsMap = new Map<string, SelectOption>();
-    let {options, noOptionMessage} = await loader.getOptions();
-    options.forEach(e => {
-      optionsMap.set(e.value, e);
-    })
-    this.setState({ isLoading: false, noOptionMessage, options: optionsMap });
+    try {
+      let options = await loader.getOptions();
+      options.forEach(e => {
+        optionsMap.set(e.value, e);
+      });
+      this.setState({ isLoading: false, hasNoOptionMessage: null, options: optionsMap });
+    } catch(e) {
+      if (e.isNoOptionsError) {
+        this.setState({ isLoading: false, hasNoOptionMessage: e.message, options: new Map() });
+      } else {
+        this.setState({ isLoading: false, hasNoOptionMessage: `No options due to: "${e}"`, options: new Map() });
+      }
+      
+    }
+    
   }
 
   render() {
@@ -298,18 +336,23 @@ export default class Control extends React.Component<Props, State> {
     const isMultiple = (field.get('multiple') || false) as boolean;
     const isClearable = !(field.get('required') || !isMultiple) as boolean;
 
-    const { isLoading, noOptionMessage , options } = this.state;
-    value = value !== undefined && value !== null ? (typeof value === "string" ? List([value]) : value) : List([])
+    const { isLoading, hasNoOptionMessage, options } = this.state;
+    value =
+      value !== undefined && value !== null
+        ? typeof value === 'string'
+          ? List([value])
+          : value
+        : List([]);
     let labeledValues = value
-    .filter(v => v !== undefined)
-    .map(v => {
-      let e = options.get(v as string);
-      if (e !== undefined && e !== null) {
-        return e;
-      } else {
-        return {value: v as string, label: v as string};
-      }
-    });
+      .filter(v => v !== undefined)
+      .map(v => {
+        let e = options.get(v as string);
+        if (e !== undefined && e !== null) {
+          return e;
+        } else {
+          return { value: v as string, label: v as string };
+        }
+      });
 
     return (
       <Creatable
@@ -320,30 +363,32 @@ export default class Control extends React.Component<Props, State> {
         options={[...options.values()]}
         getNewOptionData={e => {
           if (value.includes(e)) {
-            return {value: e, label: e};
+            return { value: e, label: e };
           } else {
-            return {value: e, label: `Create "${e}"`};
+            return { value: e, label: `Create "${e}"` };
           }
         }}
         getOptionLabel={e => e.label}
         getOptionValue={e => e.value}
-        onChange={(val: readonly SelectOption[] | SelectOption | null | undefined) => {
+        onChange={(
+          val: readonly SelectOption[] | SelectOption | null | undefined
+        ) => {
           if (Array.isArray(val)) {
-            onChange(List(val.map(e => e.value)))
+            onChange(List(val.map(e => e.value)));
           } else if (val !== null && val !== undefined) {
             onChange((val as SelectOption).value);
           } else {
             onChange(undefined);
           }
         }}
+        noOptionsMessage={() => hasNoOptionMessage === null ? 'No options' : hasNoOptionMessage}
         className={classNameWrapper}
         onFocus={setActiveStyle}
         onBlur={setInactiveStyle}
-        noOptionsMessage={() => noOptionMessage}
         styles={reactSelectStyles}
         isMulti={isMultiple}
         isClearable={isClearable}
-        placeholder=""
+        placeholder={""}
       />
     );
   }
